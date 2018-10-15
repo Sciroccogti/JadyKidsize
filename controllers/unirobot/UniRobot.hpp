6b3189cb2a347078e5e57be01163d72473a067aa
逸帆 张 <zyf199966@126.com>
2018-10-15 09:39:16 +0800

    Revert "Merge branch 'hy' into mid：增加了踢球功能"
    This reverts commit d3c78e916474b351e028a794841ddb5fb1299f23, reversing
    changes made to d14e22285c19231bc239ea9a1b45f9be52a58be0.

--- controllers/unirobot/UniRobot.hpp
+++ controllers/unirobot/UniRobot.hpp
@@ -54,11 +54,7 @@ class UniRobot : public webots::Robot {
     {
       bool ball_found;
       double ball_x, ball_y;
-<<<<<<< HEAD
 	  double direction;
-=======
-	  double direction;  // -1 is right, 1 is left
->>>>>>> hy
 	  int blueline = 0;
 	  int stepcount = 0;
 	  int bluecount = 0;
